3197a3618929b3c0ebb1dff3954c8d1be3631e12
Galina Amelina <galia.amelina@yandex.ru>
2022-08-14 21:09:39 +0300

    Refactored

--- Multithreading+serialization/PerformMethods.cs
+++ Multithreading+serialization/PerformMethods.cs
@@ -9,27 +9,33 @@ namespace Multithreading_serialization
 {
     public static class PerformMethods
     {
-        static int _counter = 10000;
+        static int _counter = 20;
         public static Semaphore sem = new Semaphore(1, 1);
+        public static int lastCount = _counter;
         public static void CountFromMillionToOne(object obj)
         {
             CancellationToken ct = (CancellationToken)obj;
-            while (_counter > 0 || !ct.IsCancellationRequested)
+            while (_counter > 0 && !ct.IsCancellationRequested)
             {
                 sem.WaitOne();
-                if (_counter >= 0)
-                {
-                    Console.WriteLine($"Счет: {_counter} , " +
-                            $"ID#: {Thread.CurrentThread.ManagedThreadId}");
-                    _counter--;
-                    Thread.Sleep(500);
-                }
-                if (ct.IsCancellationRequested)
+                lastCount = _counter;
+
+                if (ct.IsCancellationRequested || _counter < 0)
                 {
                     return;
                 }
+
+                else
+                {
+                    lastCount = _counter;
+                    Console.WriteLine($"Счет: {_counter} , ID#: {Thread.CurrentThread.ManagedThreadId}");
+
+                    _counter--;
+
+                    Thread.Sleep(200);
+                }
                 sem.Release();
-            }               
+            }
         }
 
         public static void SaveAsBinaryFormat(object objGraph, string fileName)
@@ -52,6 +58,7 @@ namespace Multithreading_serialization
             {
                 CountingData countingData = (CountingData)binFormat.Deserialize(fStream);
                 Console.WriteLine("Во сколько потоков считали в прошлый раз? : {0}", countingData.threadCount);
+                Console.WriteLine("На какой цифре остановились? : {0}", countingData.lastCount);
             }
         }
     }
--- Multithreading+serialization/Program.cs
+++ Multithreading+serialization/Program.cs
@@ -7,6 +7,8 @@ class Program
 
     public static void Main(string[] args)
     {
+        
+        
         Console.WriteLine("Сколько потоков хочешь запустить?");
 
         int threadCountRequest = 0;
@@ -14,37 +16,42 @@ class Program
         while (!int.TryParse(Console.ReadLine(), out threadCountRequest) | threadCountRequest == 0)
             Console.WriteLine("Шалите, милок! Даю вам еще попытку");
 
-        PerformMethods.sem.SafeWaitHandle.Equals(threadCountRequest);
-
         Thread[] threads = new Thread[threadCountRequest];
 
         Console.WriteLine("Как захочешь остановиться - нажми ESC");
 
         CancellationTokenSource cts = new CancellationTokenSource();
 
-        do
+        for (int i = 0; i < threadCountRequest; i++)
         {
-            for (int i = 0; i < threadCountRequest; i++)
-            {
-                if (Console.KeyAvailable)
-                {
-                    break;
-                }
-                else
-                {
-                    new Thread(new ParameterizedThreadStart(PerformMethods.CountFromMillionToOne)).Start(cts.Token);
-                }
-            }
+            var t = new Thread(new ParameterizedThreadStart(PerformMethods.CountFromMillionToOne));
+            //var t = new Thread(() => { lastCount = PerformMethods.CountFromMillionToOne(cts.Token); });
+            
+            threads[i] = t;
 
+            t.Start(cts.Token);
         }
-        while (Console.ReadKey(true).Key != ConsoleKey.Escape);
-        cts.Cancel();
 
+        var threadWorkingStates = new[] { ThreadState.WaitSleepJoin, ThreadState.Running, ThreadState.Unstarted, ThreadState.Background };
+
+        while(threads.Any(x => threadWorkingStates.Contains(x.ThreadState)) && !cts.IsCancellationRequested)
+        {
+            if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape)
+            {
+
+                cts.Cancel();
+                break;
+            }
+        }
+
+        cts.Dispose();
 
         var countingData = new CountingData();
         countingData.threadCount = threadCountRequest;
+        countingData.lastCount = PerformMethods.lastCount;
 
         PerformMethods.SaveAsBinaryFormat(countingData, "CountingData.dat");
+        
         PerformMethods.LoadFromBinaryFile("CountingData.dat");
 
         Console.ReadKey();
