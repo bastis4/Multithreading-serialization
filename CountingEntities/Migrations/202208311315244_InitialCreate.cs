b9b19c23eeb117df299dbd8049f48da1691adb97
Galina Amelina <galia.amelina@yandex.ru>
2022-08-31 17:59:20 +0300

    Add migration files

---
+++ CountingEntities/Migrations/202208311315244_InitialCreate.cs
@@ -0,0 +1,43 @@
+﻿namespace CountingEntities.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CounterItems",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ThreadCount = c.Int(nullable: false),
+                        LastCount = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Sessions",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserName = c.String(maxLength: 50),
+                        RequestDate = c.DateTime(nullable: false),
+                        RequestId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.CounterItems", t => t.RequestId)
+                .Index(t => t.RequestId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Sessions", "RequestId", "dbo.CounterItems");
+            DropIndex("dbo.Sessions", new[] { "RequestId" });
+            DropTable("dbo.Sessions");
+            DropTable("dbo.CounterItems");
+        }
+    }
+}
---
+++ CountingEntities/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace CountingEntities.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CountingEntities.CounterSessionContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "CountingEntities.CounterSessionContext";
+        }
+
+        protected override void Seed(CountingEntities.CounterSessionContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
